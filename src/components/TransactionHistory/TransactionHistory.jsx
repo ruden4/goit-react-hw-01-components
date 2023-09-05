6982a209d901376e93dc37d0ee20c096162cd9d6
ruden4 <ruden4@gmail.com>
2023-09-05 21:56:17 +0300

    done

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,5 +1,21 @@
-export const TransactionHistory = (transactions) => {
+import css from '../TransactionHistory/TransactionHistory.module.css'
+export const TransactionHistory = ({items}) => {
  return (
-    <div></div>
+    <table className={css.transactionHistory}>
+  <thead>
+    <tr>
+      <th className={css.value}>Type</th>
+      <th className={css.value}>Amount</th>
+      <th className={css.value}>Currency</th>
+    </tr>
+  </thead>
+  <tbody>
+  {items.map(item => <tr className={css.line} key={item.id}>
+      <td className={css.value}>{item.type}</td>
+      <td className={css.value}>{item.amount}</td>
+      <td className={css.value}>{item.currency}</td>
+    </tr>)}
+  </tbody>
+</table>
  )
 }
